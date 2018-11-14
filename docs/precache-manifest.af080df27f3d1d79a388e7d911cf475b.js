a4b1cc5d78762379fb17adde7d29bbac955d2ec6
Rafael Abensur <rafael@abensur.me>
2018-11-14 19:59:16 -0200

    build for prod

---
+++ docs/precache-manifest.af080df27f3d1d79a388e7d911cf475b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24924f7c8d9477fdfb4e",
+    "url": "/css/app.5d4b79a8.css"
+  },
+  {
+    "revision": "24924f7c8d9477fdfb4e",
+    "url": "/js/app.290b469a.js"
+  },
+  {
+    "revision": "6961bc37f7db05be9840",
+    "url": "/css/chunk-vendors.4509d561.css"
+  },
+  {
+    "revision": "6961bc37f7db05be9840",
+    "url": "/js/chunk-vendors.0b46b426.js"
+  },
+  {
+    "revision": "406983ef64d7f7a2b4dfaa81231df810",
+    "url": "/img/step2.406983ef.gif"
+  },
+  {
+    "revision": "b2b2f3324624d55cf4518bfa9ffafb7f",
+    "url": "/img/step1.b2b2f332.gif"
+  },
+  {
+    "revision": "6f3929536375131c28ed45f9674aaebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
